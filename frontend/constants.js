89a536c01019596ce0fa27d1123197552199fdb3
pandit <dhamdherepandit@gmail.com>
2022-10-08 12:00:02 +0530

    first commit

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/panditdhamdhere/celo-nft-marketplace";
+
+  export const MARKETPLACE_ADDRESS = "0x7684bB4b7C552255A7264E787088FE92E9B4b617";
